a944aba35c3b7a291cffdf51c6d506b29af861a8
optimistictech <olaorosamson3@gmail.com>
2022-10-31 10:46:17 +0100

    first comnit

--- me.js
+++ me.js
@@ -1,7 +1,7 @@
 module.exports = {
     "slackUsername": "optimistic", 
-    "backend": "true", 
-    "age": "26", 
+    "backend": true, 
+    "age": 26, 
     "bio": "I'm a backend developer"
    
   }
